b18a920f53f55a24ed4aa44f24674a61a18d8fb1
Phyllis Torres <ptorres@students.mchenry.edu>
2016-08-26 09:58:05 -0500

    Add files via upload

---
+++ madlibs1.py
@@ -0,0 +1,60 @@
+#Variables
+adjective = "aggressive"
+person = "librarian"
+insect = "moth"
+arachnid ="scorpion"
+flyingAnimal = "bat"
+pet = "chinchilla"
+pet2 = "pot-belly pig"
+animal = "porcupine"
+largeAnimal = "whale"
+action = "kiss"
+action2 = "burst"
+
+#poem
+print("Madlibs: There was an " + adjective + " " + person + " who " + action + "ed a " + insect + "\n")
+print("There was an " + adjective + " " + person + " who " + action + "ed a " + insect + ",")
+print("I don't know why she " + action + "ed a " + insect + " - perhaps she'll " + action2 + "!\n")
+
+print("There was an " + adjective + " " + person + " who " + action + "ed a " + arachnid + ",")
+print("That wriggled and wiggled and tickled inside her;\n")
+
+print("She " + action + "ed the " + arachnid + " to catch the " + insect + ",")
+print("I don't know why she " + action + "ed a " + insect + " - perhaps she'll " + action2 + "!\n")
+
+print("There was an " + adjective + " " + person + " who " + action + "ed a " + flyingAnimal + ";")
+print("How absurd to " + action + " a " + flyingAnimal + "!\n")
+
+print("She " + action + "ed the " + flyingAnimal + " to catch the " + arachnid + ",")
+print("She " + action + "ed the " + arachnid + " to catch the " + insect + ";")
+print("I don't know why she " + action + "ed a " + insect + " - perhaps she'll " + action2 + "!\n")
+
+print("There was an " + adjective + " " + person + " who " + action + "ed a " + pet + ";")
+print("Fancy that to " + action + " a " + pet + "!\n")
+
+print("She " + action + "ed the " + pet + " to catch the " + flyingAnimal + ",")
+print("She " + action + "ed the " + flyingAnimal + " to catch the " + arachnid + ",")
+print("She " + action + "ed the " + arachnid + " to catch the " + insect + ";")
+print("I don't know why she " + action + "ed a " + insect + " - perhaps she'll " + action2 + "!\n")
+
+print("There was an " + adjective + " " + person + " who " + action + "ed a " + pet2 + ";")
+print("What a hog to " + action + " a " + pet2 + "!\n")
+
+print("She " + action + "ed the " + pet2 + " to catch the " + pet + ",")
+print("She " + action + "ed the " + pet + " to catch the " + flyingAnimal + ",")
+print("She " + action + "ed the " + flyingAnimal + " to catch the " + arachnid + ",")
+print("She " + action + "ed the " + arachnid + " to catch the " + insect + ";")
+print("I don't know why she " + action + "ed a " + insect + " - perhaps she'll " + action2 + "!\n")
+
+print("There was an " + adjective + " " + person + " who " + action + "ed a " + animal + ";")
+print("I don't know how she " + action + "ed a " + animal + "!\n")
+
+print("She " + action + "ed the " + animal + " to catch the " + pet2 + ",")
+print("She " + action + "ed the " + pet2 + " to catch the " + pet + ",")
+print("She " + action + "ed the " + pet + " to catch the " + flyingAnimal + ",")
+print("She " + action + "ed the " + flyingAnimal + " to catch the " + arachnid + ",")
+print("She " + action + "ed the " + arachnid + " to catch the " + insect + ";")
+print("I don't know why she " + action + "ed a " + insect + " - perhaps she'll " + action2 + "!\n")
+
+print("There was an " + adjective + " " + person + " who " + action + "ed a " + largeAnimal + "...;")
+print("...She's dead, of course!")
